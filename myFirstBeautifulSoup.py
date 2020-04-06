d1dc6feeec20927087c25d4bf6265680666e6c3f
Sachin Kalamkar <kalamkar.sachin@gmail.com>
2020-04-06 16:54:09 +0530

    myFirstBeautifulSoup commited

--- myFirstBeautifulSoup.py
+++ myFirstBeautifulSoup.py
@@ -1,14 +1,22 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError
+from bs4 import BeautifulSoup
 
-try:
-	html = urlopen('https://www.youtube.com/')
-except HTTPError as e:
-	print(e)
-except URLError as e:
-	print("The server could not be found.")
-except AttributeError as e:
-	print("Tag not found.")
+def getTitle(url):
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bs = BeautifulSoup(html.read(), 'html.parser')
+		title = bs.body.h1
+	except AttributeError as e:
+		return None
+	return title
+
+
+title = getTitle("https://www.youtube.com")
+if title == None:
+	print("Title could not be found.")
 else:
-	print("It worked!")
+	print(title)
